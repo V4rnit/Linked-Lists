e254d2b2cc6f8744ee69108529583e65c0f05feb
V4rnit <121328381+V4rnit@users.noreply.github.com>
2025-01-26 23:02:59 -0500

    Create loopList.c

---
+++ loopList.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node{
+  int data;
+  struct node* next;
+}node;
+node* createNode(int val){
+  node* new = (node*)malloc(sizeof(node));
+  new->data = val;
+  new->next = NULL;
+return  new;
+}
+void displayNodes(node* head){
+  if(head == NULL){
+    printf("The linked list is empty");
+    return;
+  }
+node* traveler;
+while (traveler != NULL){
+  printf("%d",traveler->data);
+  travel = traveler->next;
+}
+  printf("The End....");
+return;
+}
+int main(){
+  node* mainNode = createNode(10);
+  return 0;
+}
